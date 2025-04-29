f04d002cffac748bbebf4c5561d357548a08cfc1
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-04-29 13:45:33 -0400

    fixed unhandled error

--- json.go
+++ json.go
@@ -30,6 +30,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
-	log.Printf("Failed to write response: %v", err)
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
 }
